7c1887bfbd7ab7e04d826b6e1420630bf1af84d5
hzrd149 <github@hzrd149.com>
2024-04-17 11:04:54 -0500

    add common relay actions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './desktop-blob-server/index.js';
 export * from './blob-downloader/index.js';
 export * from './sqlite-event-store/index.js';
 export * from './nostr-relay/index.js';
+export * from './relay-actions/index.js';
