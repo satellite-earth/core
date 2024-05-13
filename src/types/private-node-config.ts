cf9c0ccf081bebade6d779aade13d1b1a90321c0
hzrd149 <github@hzrd149.com>
2024-05-13 13:39:28 -0500

    add control api types add private node config types

---
+++ src/types/private-node-config.ts
@@ -0,0 +1,26 @@
+export type PrivateNodeConfig = {
+	owner?: string;
+	pubkeys: string[];
+	relays: { url: string }[];
+
+	cacheLevel: 1 | 2 | 3;
+
+	/**
+	 * Whether the node should require NIP-42 auth to read
+	 * Desktop: false by default
+	 * Hosted: true by default
+	 */
+	requireReadAuth: boolean;
+
+	/**
+	 * various address that this node can be reached from
+	 * Desktop: default to empty
+	 * Hosted: default to public facing URLs
+	 */
+	publicAddresses: string[];
+
+	/** @deprecated this should probably be moved to desktop */
+	autoListen: boolean;
+	/** @deprecated this should always be enabled */
+	logsEnabled: boolean;
+};
