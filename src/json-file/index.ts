428690d7831c50fd715b17ddc13c7a5459801100
hzrd149 <github@hzrd149.com>
2024-08-16 17:36:24 -0500

    removed unused export

--- src/json-file/index.ts
+++ src/json-file/index.ts
@@ -3,16 +3,6 @@ import { Adapter, Low, LowSync, SyncAdapter } from 'lowdb';
 
 import { PrivateNodeConfig } from '@satellite-earth/core/types/private-node-config.js';
 
-export const defaultConfig: PrivateNodeConfig = {
-	//pubkeys: [],
-	//relays: [],
-	//cacheLevel: 2,
-	autoListen: false,
-	logsEnabled: true,
-	requireReadAuth: false,
-	publicAddresses: [],
-};
-
 type EventMap<T> = {
 	/** fires when file is loaded */
 	loaded: [T];
